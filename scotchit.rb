9b3a741c6976006fecf47d8ad3fa5c84a5ca4838
Robert Hardin <rob@roberthardin.net>
2014-07-18 09:10:06 -0500

    Update scotchit.rb

--- scotchit.rb
+++ scotchit.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'statistics2'
 
+# Evan Miller's Wilson Score Interval fn
 def ci_lower_bound(pos, n, confidence)
     if n == 0
         return 0
@@ -20,11 +21,13 @@ CSV.foreach("ratings.csv", headers:true) do |row|
     end
     db[row['whisky name']] << ((val == nil) ? 0 : val)
 end
-#db.each {|k,i| puts "#{k}: #{i}" }
 
 db.keys.each do |k|
-    pos = db[k].count {|x| x > 90 }
-    stats[k] = [ci_lower_bound(pos, db[k].count, 0.975) * 100.0, pos]
+	if db[k].count > 5 # only eval whisky that has 6 or more reviews on reddit
+		pos = db[k].count {|x| x > 90 } # threshold as positive vote 91+% rating
+		stats[k] = [ci_lower_bound(pos, db[k].count, 0.975) * 100.0, pos, db[k].count]
+	end
 end
 
-stats.each {|k,i| (i[0] > 20.0 && (puts "#{k}: #{i}")) || nil}
+# reverse sort by confidence score and print to term
+stats.sort_by {|k,i| -i[0]}.each {|k,i| (i[0] > 20.0 && (puts "#{k}: #{i}")) || nil}
